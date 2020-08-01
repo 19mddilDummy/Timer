e0192fe2f8fc7839e21d29f6b1064bd049fab077
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2020-08-01 13:02:18 +0600

    Proposed a new schedule
    Here the new plan android development added and with it dynamic programming plan also added for 100 days.

--- time.java
+++ time.java
@@ -5,19 +5,18 @@ class time{
 		plan p1 = new plan();
 		plan p2 = new plan();
 		///use user class to set it....
-		System.out.println("\n\n114 Days for a wife");
+		System.out.println("\n\n100 Days for self progress");
 		System.out.println("Main Goal is to work 14-12 hours a day\n\n");
-		System.out.println("You are defined by your own actions.\n\n");
-		p1.set(20,2,2020,114);/// if I want to end it in 69 days from 14/06 with +12 hours
+		System.out.println("\"You are defined by your own actions.\n\n\"");
+		System.out.println("Android App development");
+		p1.set(1,8,2020,100);/// if I want to end it in 69 days from 14/06 with +12 hours
 		p1.find_ending_date();
 		p1.days_passed();
 		p1.time_remains();
 		System.out.println();
-		System.out.println("Doing web development with java and java spring as fast as possible.");
 		System.out.println();
-		System.out.println("Motives: Have a little by little plan for algorithm..\n\n");
-		System.out.println("Competitive Fire...");
-		p2.set(20,2,2020,60);
+		System.out.println("With: Dynamic Programming.\n\n");
+		p2.set(1,8,2020,10);
 		p2.find_ending_date();
 		p2.days_passed();
 		p2.time_remains();
